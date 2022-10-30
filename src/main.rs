1261861c2512a0e7867055729ea00457ca100561
Nathan Do <nathan.do@alumni.virginia.edu>
2022-10-30 11:43:28 -0400

    fix: readme to include new features

--- src/main.rs
+++ src/main.rs
@@ -26,26 +26,29 @@ A HEx Helper to edit bytes by the nibble.
 Do --help for more information.";
 
 const LONG_ABOUT: &str = "
-The HEx Helper is a terminal tool used for modifying binaries by
-the nibble. It aims to replicate some of the look of hexyl while
-functionaly acting like a terminal UI version of GHex.
+The HEx Helper is a terminal tool used for modifying binaries by the nibble.
+It aims to replicate some of the look of hexyl while functionaly acting like
+a terminal UI version of GHex.
 
-Note that the octal and hexadecimal labels are slightly
-different in heh; they interpret the stream as if 0s were filled
-to the end of the byte (i.e. stream length 9 on FF FF would
-produce octal 377 200 and hexadecimal FF 80).
+Note that the octal and hexadecimal labels are slightly different in heh; they
+interpret the stream as if 0s were filled to the end of the byte (i.e. stream
+length 9 on FF FF would produce octal 377 200 and hexadecimal FF 80).
 
 Like GHex, you cannot create files with heh, only modify them.
 
 Terminal UI Commands:
-    ALT=                Increase the stream length by 1
-    ALT-                Decrease the stream length by 1
-    CNTRLs              Save
-    CNTRLq              Quit
-    CNTRLj              Jump to Byte
+    ALT =               Increase the stream length by 1
+    ALT -               Decrease the stream length by 1
+    CTRL c              Copy Selection in Current Editor (Hex or ASCII)
+    CTRL ALT c          Copy Selection in Other Editor
+    CTRL j              Jump to Byte
+    CTRL q              Quit
+    CTRL s              Save
+    CTRL z              Undo
 
 Left-clicking on a label will copy the contents to the clipboard.
-Left-clicking on the ASCII or hex table will focus it.
+Left-clicking on the ASCII or hex table will focus it. Dragging the click will
+select the specified text.
 
 Zooming in and out will change the size of the components.";
 
