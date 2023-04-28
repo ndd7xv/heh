27d637f9c2801f67be3831b8ee661f685e80884f
Josh Harcombe <joshharc@gmail.com>
2023-04-28 23:32:37 +1000

    clear notifications after successful actions + fix typos

--- src/app.rs
+++ src/app.rs
@@ -206,9 +206,11 @@ impl Application {
         let event = event::read()?;
         match event {
             Event::Key(key) => {
+                self.labels.notification.clear();
                 return input::handle_key_input(self, key);
             }
             Event::Mouse(mouse) => {
+                self.labels.notification.clear();
                 input::handle_mouse_input(self, mouse);
             }
             Event::Resize(_, _) | Event::FocusGained | Event::FocusLost | Event::Paste(_) => {}
